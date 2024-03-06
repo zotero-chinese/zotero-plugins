d460c05fe97ae65ab22ce32bb8540f55ba0b1552
Northword <northword@outlook.com>
2024-03-06 23:19:21 +0800

    Fix download link

--- src/components/download-modal.vue
+++ src/components/download-modal.vue
@@ -1,68 +1,76 @@
 <!-- DownloadModal.vue -->
+
 <template>
-    <div class="modal" v-if="showModal">
-      <div class="modal-content">
-        <span class="close" @click="closeModal">&times;</span>
-        <h2>Downloads</h2>
-        <ul>
-          <li v-for="release in selectedPlugin.releases" :key="release.targetZoteroVersion">
-            <p>Zotero Version: {{ release.targetZoteroVersion }}</p>
-            <p>Download: {{ release.tagName }}</p>
-          </li>
-        </ul>
-      </div>
+  <div class="modal" v-if="showModal">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h2>Downloads</h2>
+      <ul>
+        <li v-for="release in selectedPlugin.releases" :key="release.targetZoteroVersion">
+          <p>适配 Zotero 版本: {{ release.targetZoteroVersion }}</p>
+          <p>插件版本：{{ release.tagName }}</p>
+          <p>下载链接:
+          <ul>
+            <li v-for="(value, key) in release.xpiDownloadUrl">
+              <p>{{ key }}: {{ value }}</p>
+              <!-- <a href={{ value }}>{{ key }}</a> -->
+            </li>
+          </ul>
+          </p>
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "DownloadModal",
-    props: {
-      showModal: Boolean,
-      selectedPlugin: Object
-    },
-    methods: {
-      closeModal() {
-        this.$emit("closeModal");
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DownloadModal",
+  props: {
+    showModal: Boolean,
+    selectedPlugin: Object
+  },
+  methods: {
+    closeModal() {
+      this.$emit("closeModal");
     }
-  };
-  </script>
-  
-  <style scoped>
-  .modal {
-    /* display: none; */
-    position: fixed;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    background-color: rgb(0,0,0);
-    background-color: rgba(0,0,0,0.4);
   }
-  
-  .modal-content {
-    background-color: #fefefe;
-    margin: 15% auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-  }
-  
-  .close {
-    color: #aaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-  }
-  
-  .close:hover,
-  .close:focus {
-    color: black;
-    text-decoration: none;
-    cursor: pointer;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.modal {
+  /* display: none; */
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
