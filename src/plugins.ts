edcac3a5eeb81716c2e7675efcd4c3b42d5e8709
syt <dreamcontinue.cd@gmail.com>
2024-12-23 23:02:41 +0800

    Update plugins.ts

--- src/plugins.ts
+++ src/plugins.ts
@@ -109,6 +109,16 @@ export const plugins: PluginInfoBase[] = [
     tags: ['integration'],
   },
   {
+    repo: 'diegodlh/zotero-cita',
+    releases: [
+      {
+        targetZoteroVersion: '7',
+        tagName: 'pre',
+      },
+    ],
+    tags: ['integration'],
+  },
+  {
     repo: 'Dominic-DallOsto/zotero-annotations-count',
     releases: [
       {
