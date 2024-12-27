60f750dbb89a99025985b91c47f5909ffb9b4a24
juju <296768324@qq.com>
2024-12-27 17:59:30 +0800

    Add zotero-pdf-background (#152)
    * add plugin zotero-pdf-background
    
    * add plugin zotero-pdf-background;sorted by repo;
    
    * lint
    
    ---------
    
    Co-authored-by: juju <juju@296768324.com>

--- src/plugins.ts
+++ src/plugins.ts
@@ -433,6 +433,20 @@ export const plugins: PluginInfoBase[] = [
     tags: ['ai', 'productivity'],
   },
   {
+    repo: 'q77190858/zotero-pdf-background',
+    releases: [
+      {
+        targetZoteroVersion: '7',
+        tagName: 'latest',
+      },
+      {
+        targetZoteroVersion: '6',
+        tagName: 'v0.0.2',
+      },
+    ],
+    tags: ['reader'],
+  },
+  {
     repo: 'redleafnew/delitemwithatt',
     releases: [
       {
