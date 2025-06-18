d2305a0d5807153a47337b81f7dec791ad76f1ad
syt <dreamcontinue.cd@gmail.com>
2025-06-18 19:57:52 +0800

    Add zotero-citation-tally, update pubpeer_zotero_plugin (#186)
    * Update plugins.ts
    
    add zotero-citation-tally, update pubpeer_zotero_plugin
    
    * Update deprecated.ts
    
    remove pubpeer_zotero_plugin in deprecated.ts
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: Northword <44738481+northword@users.noreply.github.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/plugins.ts
+++ src/plugins.ts
@@ -115,6 +115,16 @@ export const plugins: PluginInfoBase[] = [
     tags: ['metadata'],
   },
   {
+    repo: 'daeh/zotero-citation-tally',
+    releases: [
+      {
+        targetZoteroVersion: '7',
+        tagName: 'latest',
+      },
+    ],
+    tags: ['metadata'],
+  },
+  {
     repo: 'daeh/zotero-markdb-connect',
     releases: [
       {
@@ -504,6 +514,16 @@ export const plugins: PluginInfoBase[] = [
       },
     ],
     tags: ['ai', 'productivity'],
+  },
+  {
+    repo: 'PubPeerFoundation/pubpeer_zotero_plugin',
+    releases: [
+      {
+        targetZoteroVersion: '7',
+        tagName: 'latest',
+      },
+    ],
+    tags: ['integration'],
   },
   {
     repo: 'q77190858/zotero-pdf-background',
