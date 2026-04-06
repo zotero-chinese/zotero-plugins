14961749ec0f0ed91234254ccf5b304854f42d63
Northword <northword@outlook.com>
2026-04-06 15:45:28 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  typescript: {
+    overrides: {
+      'e18e/prefer-static-regex': 'off',
+    },
+  },
+})
