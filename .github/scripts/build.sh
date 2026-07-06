23bac3c5a90fa2b66fc714f98106f3c34b51e279
Northword <northword@outlook.com>
2026-07-06 19:14:27 +0800

    Disable charts workflow run

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -11,12 +11,17 @@ set -e
 dist="dist"
 
 # 获取数据
-# pnpm run data:info
+## pnpm run data:info
 if [ ! -d "$dist" ]; then
     mkdir $dist
 fi
+
 curl -L -o "$dist/plugins.json" "https://github.com/syt2/zotero-addons-scraper/releases/latest/download/addon_infos.json"
-pnpm run data:chart
+
+## 2026-7-6: GitHub API 故障，listStargazersForRepo 今年返回有写权限仓库的 stargazers，余者均为 404，
+## 临时禁用 charts 数据获取，保持旧数据缓存
+curl -L -o "$dist/charts.json" "https://github.com/zotero-chinese/zotero-plugins/raw/refs/heads/gh-pages/charts.json"
+# pnpm run data:chart
 
 
 # 复制兼容性文件
