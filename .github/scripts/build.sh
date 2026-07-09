8c0efaa186ffa71b67db0f73ad5f82193e19288c
Northword <northword@outlook.com>
2026-07-09 12:55:20 +0800

    Add authorization to curl

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -16,11 +16,11 @@ if [ ! -d "$dist" ]; then
     mkdir $dist
 fi
 
-curl -L -o "$dist/plugins.json" "https://github.com/syt2/zotero-addons-scraper/releases/latest/download/addon_infos.json"
+curl -H "Authorization: bader $process.env.GITHUB_TOKEN" -L -o "$dist/plugins.json" "https://github.com/syt2/zotero-addons-scraper/releases/latest/download/addon_infos.json"
 
 ## 2026-7-6: GitHub API 故障，listStargazersForRepo 今年返回有写权限仓库的 stargazers，余者均为 404，
 ## 临时禁用 charts 数据获取，保持旧数据缓存
-curl -L -o "$dist/charts.json" "https://github.com/zotero-chinese/zotero-plugins/raw/refs/heads/gh-pages/charts.json"
+curl -H "Authorization: bader $process.env.GITHUB_TOKEN" -L -o "$dist/charts.json" "https://github.com/zotero-chinese/zotero-plugins/raw/refs/heads/gh-pages/charts.json"
 # pnpm run data:chart
 
 
